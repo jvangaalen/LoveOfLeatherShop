18a79ab54e6bdeeab98396d22ec296908a4d52ea
jvangaalen <jerome.vangaalen@gmail.com>
2018-07-19 21:02:39 -0400

    Added a database folder, added a DataBaseInitializer, added a repository. Added CRUD operations to repository. Moved Context.cs to Data folder,

---
+++ LoveOfLeatherShop/Data/DataBaseInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoveOfLeatherShop.DAL
+{
+    public class DataBaseInitializer
+    {
+    }
+}
